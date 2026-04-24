6badba68f78286871fb892317d454562cc6c98e7
inc <inc@email.com>
2026-04-25 01:04:21 +0300

    fix: staticfiles

--- scriba/settings.py
+++ scriba/settings.py
@@ -169,7 +169,7 @@ ACCOUNT_USERNAME_BLACKLIST = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
