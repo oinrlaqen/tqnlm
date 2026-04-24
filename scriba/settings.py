0eb6866f7d960ff4953d2b2200069bb0e35d33f3
inc <inc@email.com>
2026-04-24 19:53:39 +0300

    deploy

--- scriba/settings.py
+++ scriba/settings.py
@@ -38,9 +38,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tqnlm-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://tqnlm-production.up.railway.app']
 
 # Force HTTPS-only cookies
 SESSION_COOKIE_SECURE = True
